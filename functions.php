08abb0633d68e75265e678ae1de83b7d9c45aaa2
Saniyaj Mallik <saniyajmallik12@gmail.com>
2025-02-06 12:43:56 +0530

    File and theme setup complete

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Storefront engine room
+ *
+ * @package storefront
+ */
+
+/**
+ * Enque theme style .
+ */
+function my_child_theme_enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'child-style', get_stylesheet_uri(), array( 'parent-style' ) );
+}
+add_action( 'wp_enqueue_scripts', 'my_child_theme_enqueue_styles' );
---
+++ template-custom.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying Custom pages.
+ *
+ * Template Name: Custom Template
+ *
+ * @package storefront
+ */
+
+?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<h1>Custom Page template</h1>
+			<img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/images/box.png' ); ?>" alt="">
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+
